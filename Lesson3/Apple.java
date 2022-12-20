7cc57f5316b338f6b7ed5c8533af8ef1770c0473
Goncharov <iiiyruk97@mail.ru>
2022-12-20 17:06:43 +0300

    Lesson3

---
+++ Lesson3/Apple.java
@@ -0,0 +1,9 @@
+package JK.Lesson3;
+
+public class Apple extends Fruit {
+    Apple () {super(1.0f);
+    }
+    public String toString(){
+        return "apple";
+    }
+}
---
+++ Lesson3/Orange.java
@@ -0,0 +1,9 @@
+package JK.Lesson3;
+
+public class Orange extends Fruit {
+    Orange () {super(1.5f);
+    }
+    public String toString(){
+        return "orange";
+    }
+}
